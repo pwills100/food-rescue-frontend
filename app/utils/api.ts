1a39c957fad95e07e4e5b4fc0c1712d1ee1be0c9
pwills100 <princewill31@gmail.com>
2026-09-10 16:36:35 +0100

    Add fallback API URL

--- app/utils/api.ts
+++ app/utils/api.ts
@@ -1,4 +1,4 @@
-const API_BASE = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000';
+const API_BASE = process.env.NEXT_PUBLIC_API_URL || 'https://foodrescue.vote40andfabulousnaija.com';
 
 export async function fetchFromApi(endpoint: string, options?: RequestInit) {
   const res = `${API_BASE}${endpoint}`;
